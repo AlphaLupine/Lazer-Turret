05130a7587c8a2fcacf391988fb3dc63ada2f661
Matthew <matthewduff0912@gmail.com>
2022-01-20 10:19:15 +0000

    Add project files.

---
+++ LaserTurret2/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace LaserTurret2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.portSelector = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.portToggle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.portSelector)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serialPort
+            // 
+            this.serialPort.PortName = "COM7";
+            // 
+            // portSelector
+            // 
+            this.portSelector.Location = new System.Drawing.Point(3, 529);
+            this.portSelector.Maximum = new decimal(new int[] {
+            11,
+            0,
+            0,
+            0});
+            this.portSelector.Name = "portSelector";
+            this.portSelector.Size = new System.Drawing.Size(120, 20);
+            this.portSelector.TabIndex = 0;
+            this.portSelector.ValueChanged += new System.EventHandler(this.portSelector_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 513);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "COM Port";
+            // 
+            // portToggle
+            // 
+            this.portToggle.Location = new System.Drawing.Point(129, 529);
+            this.portToggle.Name = "portToggle";
+            this.portToggle.Size = new System.Drawing.Size(157, 23);
+            this.portToggle.TabIndex = 2;
+            this.portToggle.Text = "Close Port";
+            this.portToggle.UseVisualStyleBackColor = true;
+            this.portToggle.Click += new System.EventHandler(this.portToggle_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.portToggle);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.portSelector);
+            this.Name = "Form1";
+            this.Text = "-";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.portSelector)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort port;
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.NumericUpDown portSelector;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button portToggle;
+    }
+}
+
---
+++ LaserTurret2/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace LaserTurret2
+{
+    public partial class Form1 : Form
+    {
+
+        bool isFound;
+
+        public Stopwatch watch { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            serialPort.PortName = String.Format("COM{0}", portSelector.Value);
+            watch = Stopwatch.StartNew();
+            try
+            {
+                serialPort.Open();
+                isFound = true;
+                portToggle.Text = "Turn Port Off";
+            }
+            catch 
+            {
+                isFound = false;
+                portToggle.Text = "PORT NOT FOUND";
+            }
+            
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+           
+            if(isFound) writeToPort(new Point(e.X, e.Y));
+        }
+
+        public void writeToPort(Point coords) {
+            if (watch.ElapsedMilliseconds < 15) return;
+            watch = Stopwatch.StartNew();
+            serialPort.Write(String.Format("X{0}Y{1}",
+                (coords.X / (Size.Width / 180)),
+                (coords.Y / (Size.Height / 180))));
+        }
+
+        private void portSelector_ValueChanged(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen) {
+                serialPort.Close();
+                portToggle.Text = "Turn Port On";
+            }
+            serialPort.PortName = String.Format("COM{0}", portSelector.Value);
+            try
+            {
+                serialPort.Open();
+                portToggle.Text = "Turn Port Off";
+                isFound = true;
+            }
+            catch 
+            {
+                portToggle.Text = "PORT NOT FOUND";
+                isFound = false;
+            }
+       
+        }
+
+        private void portToggle_Click(object sender, EventArgs e)
+        {
+            if (!isFound) return;
+
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close();
+                isFound = false;
+                portToggle.Text = "Turn Port On";
+            }
+            else 
+            {
+                serialPort.Open();
+                portToggle.Text = "Turn Port Off";
+            }
+        }
+    }
+}
